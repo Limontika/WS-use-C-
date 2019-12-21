d0a3c379d331895d6a8434253f32d09ea2766350
Limontika <48252591+Limontika@users.noreply.github.com>
2019-12-21 22:15:18 +0300

    Форма принятия изделий готова

--- WindowsFormsApp1/WindowsFormsApp1/receipt_of_materials.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/receipt_of_materials.Designer.cs
@@ -43,6 +43,22 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -50,7 +66,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Location = new System.Drawing.Point(15, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(123, 13);
             this.label1.TabIndex = 0;
@@ -60,27 +76,29 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(15, 40);
+            this.comboBox1.Location = new System.Drawing.Point(18, 40);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(178, 21);
+            this.comboBox1.Size = new System.Drawing.Size(193, 21);
             this.comboBox1.TabIndex = 1;
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(243, 40);
+            this.comboBox2.Location = new System.Drawing.Point(299, 40);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(193, 21);
+            this.comboBox2.Size = new System.Drawing.Size(63, 21);
             this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(240, 24);
+            this.label2.Location = new System.Drawing.Point(296, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(66, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Количество";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
@@ -106,6 +124,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(175, 20);
             this.textBox1.TabIndex = 7;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // textBox2
             // 
@@ -120,6 +139,15 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column8,
+            this.Column6,
+            this.Column7});
             this.dataGridView1.Location = new System.Drawing.Point(12, 102);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
@@ -134,6 +162,7 @@
             this.button1.TabIndex = 10;
             this.button1.Text = "Добавить";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -143,12 +172,25 @@
             this.button2.TabIndex = 11;
             this.button2.Text = "Заказать";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // dataGridView2
             // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column9,
+            this.Column10,
+            this.Column16,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14,
+            this.Column15});
             this.dataGridView2.Location = new System.Drawing.Point(12, 328);
             this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
             this.dataGridView2.Size = new System.Drawing.Size(858, 197);
             this.dataGridView2.TabIndex = 12;
             // 
@@ -160,6 +202,7 @@
             this.button3.TabIndex = 13;
             this.button3.Text = "Подтвердить";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -179,6 +222,104 @@
             this.label5.Size = new System.Drawing.Size(56, 13);
             this.label5.TabIndex = 15;
             this.label5.Text = "Поставки";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "id";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "composition";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "color";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "width";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "height";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Количество";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "cost";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "vendor_code";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Visible = false;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "id";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "composition";
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "color";
+            this.Column16.Name = "Column16";
+            this.Column16.ReadOnly = true;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "width";
+            this.Column11.Name = "Column11";
+            this.Column11.ReadOnly = true;
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "height";
+            this.Column12.Name = "Column12";
+            this.Column12.ReadOnly = true;
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "Количество";
+            this.Column13.Name = "Column13";
+            this.Column13.ReadOnly = true;
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "cost";
+            this.Column14.Name = "Column14";
+            this.Column14.ReadOnly = true;
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "vendor_code";
+            this.Column15.Name = "Column15";
+            this.Column15.ReadOnly = true;
+            this.Column15.Visible = false;
             // 
             // receipt_of_materials
             // 
@@ -202,6 +343,7 @@
             this.Controls.Add(this.label1);
             this.Name = "receipt_of_materials";
             this.Text = "Материалы";
+            this.Load += new System.EventHandler(this.receipt_of_materials_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
@@ -226,5 +368,21 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/receipt_of_materials.cs
+++ WindowsFormsApp1/WindowsFormsApp1/receipt_of_materials.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,10 @@ namespace WindowsFormsApp1
 {
     public partial class receipt_of_materials : Form
     {
+
+        private int cost;
+        private int rows = 1;
+
         public receipt_of_materials()
         {
             InitializeComponent();
@@ -25,6 +30,222 @@ namespace WindowsFormsApp1
         private void button4_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void receipt_of_materials_Load(object sender, EventArgs e)
+        {
+            List<object> materials = new List<object>();
+
+            this.comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            MySqlConnection conn = DB.GetDBConnection();
+            try
+            {
+                conn.Open();
+            }
+            catch
+            {
+                MessageBox.Show(
+                "Проблемы с подключением к БД",
+                "Сообщение",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1);
+            }
+
+            try
+            {
+                string sql = $"SELECT vendor_code, composition, color, width, height FROM rolls";
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sql;
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    materials.Add(reader[1] + " " + reader[2]);
+                }
+                reader.Close();
+                conn.Close();
+                comboBox1.Items.AddRange(materials.ToArray());
+            }
+            catch
+            {
+                MessageBox.Show(
+                "Данные не найдены",
+                "Сообщение",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1);
+            }
+
+            object[] quanyti = new object[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            comboBox2.Items.AddRange(quanyti);
+            comboBox2.SelectedIndex = 0;
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = DB.GetDBConnection();
+            try
+            {
+                conn.Open();
+            }
+            catch
+            {
+                MessageBox.Show(
+                "Проблемы с подключением к БД",
+                "Сообщение",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1);
+            }
+
+            int index = 0;
+            int quanyti = 0;
+            int height = 0;
+            int width = 0;
+            int vendor_code = 0;
+            string name = "";
+            string color = "";
+
+            int.TryParse(comboBox1.SelectedIndex.ToString(), out index);
+            int.TryParse(comboBox2.SelectedItem.ToString(), out quanyti);
+
+            try
+            {
+                string sql = $"SELECT vendor_code, composition, color, width, height FROM rolls WHERE vendor_code = {index + 1}";
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sql;
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    name = reader[1].ToString();
+                    color = reader[2].ToString();
+                    int.TryParse(reader[3].ToString(), out width);
+                    int.TryParse(reader[4].ToString(), out height);
+                    int.TryParse(reader[0].ToString(), out vendor_code);
+                }
+                reader.Close();
+                conn.Close();
+
+            }
+            catch
+            {
+                MessageBox.Show(
+                "Не выбрано материалов для оформления заказа",
+                "Сообщение",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1);
+            }
+
+
+            int temp = int.Parse(textBox1.Text) * quanyti;
+            cost += temp;
+
+            textBox2.Text = cost.ToString();
+
+            int i = 0;
+
+            while (i != dataGridView1.RowCount)
+            {
+                if (int.Parse(dataGridView1.Rows[i].Cells[4].Value.ToString()) == vendor_code)
+                {
+                    dataGridView1.Rows[i].Cells[2].Value = quanyti + int.Parse(dataGridView1.Rows[i].Cells[2].Value.ToString());
+                    return;
+                }
+                i++;
+            }
+
+
+            dataGridView1.Rows.Add(rows, comboBox1.SelectedItem.ToString(), color, width, height, quanyti, temp, vendor_code);
+            rows += 1;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int count = dataGridView1.RowCount;
+
+            try
+            {
+
+                foreach (DataGridViewRow r in dataGridView1.Rows)
+                {
+                    int index = dataGridView2.Rows.Add(r.Clone() as DataGridViewRow);
+                    foreach (DataGridViewCell o in r.Cells)
+                    {
+                        dataGridView2.Rows[index].Cells[o.ColumnIndex].Value = o.Value;
+                    }
+                }
+
+                dataGridView1.Rows.Clear();
+
+                rows = 1;
+
+                MessageBox.Show(
+                "Заказ успешно создан",
+                "Сообщение",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1);
+            }
+
+            catch (Exception er)
+            {
+                MessageBox.Show(
+                $"Не удалось передать заказ, повторите попытку (Ошибка {er})",
+                "Сообщение",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView2.Rows.Clear();
+
+                MessageBox.Show(
+                "Принято на склад",
+                "Сообщение",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1);
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(
+                $"Что-то пошло не так (Ошибка {er})",
+                "Сообщение",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1);
+            }
         }
     }
 }
